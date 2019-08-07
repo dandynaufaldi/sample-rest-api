2161a95042b2679a57188e59bdc0c351d29613b8
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2019-08-07 15:26:26 +0700

    patch add publication to article

--- models/article.go
+++ models/article.go
@@ -4,11 +4,12 @@ import "time"
 
 // Article reprsent the article model
 type Article struct {
-	ID        int64     `json:"id"`
-	Slug      string    `json:"slug"`
-	Title     int64     `json:"title"`
-	Author    Author    `json:"author"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          int64       `json:"id"`
+	Slug        string      `json:"slug"`
+	Title       int64       `json:"title"`
+	Author      Author      `json:"author"`
+	Publication Publication `json:"publication,omitempty"`
+	Content     string      `json:"content"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
 }
