777ab4d29f1b59434e95cf6c11bc25e0096124a8
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2019-08-07 15:13:09 +0700

    add models: article, author, publication

---
+++ models/author.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+// Author represent author model
+type Author struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
